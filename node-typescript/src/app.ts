7736964f9453bb00ebce2249b9ad15377c26d64b
Your Name <soren.skieller@gmail.com>
2019-08-30 21:33:38 +0200

    added more lesson1 work

--- node-typescript/src/app.ts
+++ node-typescript/src/app.ts
@@ -2,15 +2,28 @@ import http from 'http';
 import httpStatus from 'http-status-codes';
 
 const port = 3000;
-const app = http.createServer( (request, response) =>{
-    console.log( 'Received an incoming request!' );
- response.writeHead( httpStatus.OK, {
- 'Content-Type': 'text/html'
- } );
- let responseMessage = '<h1>Hello Node, TypeScript speaking! test2</h1>';
- response.write( responseMessage );
- response.end();
- console.log( `Sent a response : ${responseMessage}` );
- } );
-app.listen( port );
-console.log( `The server has started and is listening on port number: ${port}` );
+
+const routeResponseMap = {
+    "/info" : "<h1>Info Page</h1>",
+    "/error" : "<h1>404: This page cannot be found.</h1>",
+};
+
+
+const app = http.createServer((request, response) => {
+    console.log('Received an incoming request!');
+    response.writeHead(httpStatus.OK, {
+        'Content-Type': 'text/html'
+    });
+    
+    if (request.url != null && request.url in routeResponseMap){
+        response.end(routeResponseMap )
+    } else {
+        response.end("<h1>Welcome</h1>");
+    }
+
+});
+
+
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
