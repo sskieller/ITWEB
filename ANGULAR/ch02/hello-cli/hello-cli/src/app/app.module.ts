bbb37b46dfca944882a06d9f5dacf1319e53ceb4
Søren <soren.skieller@gmail.com>
2019-10-02 19:54:34 +0200

    wat

--- ANGULAR/ch02/hello-cli/hello-cli/src/app/app.module.ts
+++ ANGULAR/ch02/hello-cli/hello-cli/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { ShippingModule } from './shipping/shipping.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ShippingModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
